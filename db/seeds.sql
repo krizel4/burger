b3bd8b76adfb00de5ad1cfce02873e0e3cd3e9a4
Krizel <krizzlybear@Minnemac.local>
2020-01-20 22:14:00 -0800

    adding initial files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('California', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Western', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Cheeseburger', true);
