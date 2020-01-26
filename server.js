5d0413d3d74033f0756797fc13fcb304e88aa056
Krizel Minnema <krizzlybear@Krizels-MacBook-Pro.local>
2020-01-25 17:33:48 -0800

    adding connection and routes in server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 const express = require('express');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// Static cntent
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// Set handlebars
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main' }));
+    app.set('view engine', 'handlebars');
+
+// import routes and give server access
+const routes = require('./controllers/burgers_controller.js');
+
+app.use(routes);
+
+// start server to start listening to requests
+app.listen(PORT, function() {
+    console.log('Server listening on: http://host:' + PORT);
+});
+
