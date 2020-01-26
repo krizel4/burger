ab3466c56f66d84d663b241bef6f29e7af84aa2d
Krizel Minnema <krizzlybear@Krizels-MacBook-Pro.local>
2020-01-25 18:34:34 -0800

    adding public files and handlebars

--- models/burger.js
+++ models/burger.js
@@ -3,29 +3,27 @@ const orm = require('../config/orm.js');
 // call all ORM functions using specific input for the ORM
 var burger = {
     all: function(cb) {
-        orm.all('burgers', function(res) {
-            cb(res);
-        });
+      orm.all('burgers', function(res) {
+        cb(res);
+      });
     },
-    // create
+    // The variables cols and vals are arrays.
     create: function(cols, vals, cb) {
-        orm.create('burgers', cols, vals, function(res) {
-            cb(res);
-        });
+      orm.create('burgers', cols, vals, function(res) {
+        cb(res);
+      });
     },
-    // update
     update: function(objColVals, condition, cb) {
-        orm.update('burgers', objColVals, condition, function(res) {
-            cb(res);
-        });
+      orm.update('burgers', objColVals, condition, function(res) {
+        cb(res);
+      });
     },
-    // delete
     delete: function(condition, cb) {
-        orm.delete('burgers', condition, function(res) {
-            cb(res);
-        });
+      orm.delete('burgers', condition, function(res) {
+        cb(res);
+      });
     }
-};
+  };
 
 // export the burger.js file
 module.exports = burger;
