edcd27e0746400db0f3fc92ad7f1039d4387cfc5
Krizel Minnema <krizzlybear@Krizels-MacBook-Pro.local>
2020-01-25 16:58:45 -0800

    adding orm functions

--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,31 @@
 const orm = require('..config/orm.js');
 
 // call all ORM functions using specific input for the ORM
-
-// create
-// update
-// delete
+let burger = {
+    all: function(cb) {
+        orm.all('burgers', function(res) {
+            cb(res);
+        });
+    },
+    // create
+    create: function(cols, vals, cb) {
+        orm.create('burgers', cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    // update
+    update: function(objColVals, condition, cb) {
+        orm.update('burgers', objColVals, condition, function(res) {
+            cb(res);
+        })
+    },
+    // delete
+    delete: function(objColVals, condition, cb) {
+        orm.update('burgers', objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+};
 
 // export the burger.js file
+module.exports = burger;
