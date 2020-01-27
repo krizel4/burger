0447a7d75b6c5cb24eb1269ed0fc513311ec5571
Krizel Minnema <krizzlybear@Krizels-MacBook-Pro.local>
2020-01-26 16:59:02 -0800

    methods were changed in controller. resolving version control issues as a result of working on multiple devices

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,13 +5,14 @@ const router = express.Router();
 
 // import the model to use its db functions
 const burger = require('../models/burger.js');
+
 // =============
 // ROUTERS
 // =============
 
 // all
 router.get('/', function(req, res) {
-    burger.all(function(data) {
+    burger.selectAll(function(data) {
         let hbsObject = {
             burgers: data
         };
@@ -22,11 +23,13 @@ router.get('/', function(req, res) {
 
 // create
 router.post('/api/burgers', function(req, res) {
-    burger.create ([
-        'burger_name', 'devoured'
+    console.log(res.body);
+    burger.insertOne ([
+        'burger_name'
     ], [
-        req.body.name, req.body.sleepy
+        req.body.burger_name
     ], function(result) {
+        console.log(result);
         // send back the ID of the new quote
         res.json({ id: result.insertID });
     });
@@ -34,28 +37,14 @@ router.post('/api/burgers', function(req, res) {
 
 // put
 router.put('/api/burgers:id', function(req, res) {
-    var condition = 'id = ' + req.params.id;
+    let condition = 'id = ' + req.params.id;
     console.log('condition', condition);
 
 // update
-    burger.update({
+    burger.updateOne({
         devoured: req.body.devoured
     }, condition, function(result) {
         if (result.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist so 404
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
-});
-
-// delete
-router.delete('/api/burgers/:id', function(req, res) {
-    const condition = 'id = ' + req.params.id;
-
-    burger.delete(condition, function(result) {
-        if (result.affectedRows = 0) {
             // If no rows were changed, then the ID must not exist so 404
             return res.status(404).end();
         } else {
--- models/burger.js
+++ models/burger.js
@@ -2,28 +2,23 @@ const orm = require('../config/orm.js');
 
 // call all ORM functions using specific input for the ORM
 var burger = {
-    all: function(cb) {
-      orm.selectAll('burgers', function(res) {
-        cb(res);
-      });
-    },
-    // The variables cols and vals are arrays.
-    create: function(cols, vals, cb) {
-      orm.insertOne('burgers', cols, vals, function(res) {
-        cb(res);
-      });
-    },
-    update: function(objColVals, condition, cb) {
-      orm.updateOne('burgers', objColVals, condition, function(res) {
-        cb(res);
-      });
-    },
-    delete: function(condition, cb) {
-      orm.deleteOne('burgers', condition, function(res) {
-        cb(res);
-      });
-    }
-  };
+  selectAll: function (cb) {
+    orm.selectAll('burgers', function (res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function (cols, vals, cb) {
+    orm.insertOne('burgers', cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  updateOne: function (objColVals, condition, cb) {
+    orm.updateOne('burgers', objColVals, condition, function (res) {
+      cb(res);
+    });
+  }
+};
 
 // export the burger.js file
 module.exports = burger;
